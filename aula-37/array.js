1b3e66b57f2442ec799e31bf1b8b48ebfb1aada3
Car <carloslcr@hotmail.com>
2025-01-05 23:34:14 -0300

    sla

---
+++ aula-37/array.js
@@ -0,0 +1,8 @@
+const listaDeCompras = ['macarrão', false]
+console.log(listaDeCompras)
+
+listaDeCompras[0] = 'arroz'
+listaDeCompras[1] = 'feijão'
+listaDeCompras[2] = '7'
+
+console.log(listaDeCompras)
